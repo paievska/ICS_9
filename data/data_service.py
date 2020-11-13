e7ebba5ad35e3443d0b963c7b8d094de096eb0f7
Paievska <dianapaievska111@gmail.com>
2020-11-13 17:25:26 +0200

    commit

--- data/data_service.py
+++ data/data_service.py
@@ -2,24 +2,26 @@
 
 
 
-def get_dovidnik():
+def get_dovidniks():
     """повертає список клієтів з файла 'dovidnik.txt'
     
     Returns:
-        dovidnik_list : список ринків
+        dovidniks_list : список ринків
     """
     
-    with open('./data/dovidnik.txt') as dovidnik_file:
-        from_file = dovidnik_file.readlines()
+    with open('dovidniks.txt') as dovidniks_file:
+        from_file = dovidniks_file.readlines()
+    
+    #накопичування елеменів довідника
    
-    dovidnik_list = []
+    dovidniks_list = []
 
     for line in from_file:
         line_list = line.split(';')
-        dovidnik_list.append((line_list))
+        dovidniks_list.append((line_list))
     
     
-    return dovidnik_list
+    return dovidniks_list
 
 
 
@@ -33,10 +35,10 @@ def get_statistics():
         statistics_list : список статистики данних ринків
     """
 
-    with open('./data/statistics.txt') as statistics_file:
+    with open('statistics.txt') as statistics_file:
         from_file = statistics_file.readlines()
 
-    #накопичувач статистик данних ринків
+    #накопичувач статистичних даних ринків
 
     statistics_list = []
 
@@ -49,16 +51,16 @@ def get_statistics():
 
 
 
-def show_dovidnik(dovidnik):
+def show_dovidniks(dovidniks):
     """виводить елементи довідника на екран
 
     Args:
         dovidnik (list): список елементів
     """
 
-    for c in dovidnik:
-        print("код ринка:{:3} назва ринку: {:3}"
-            .format(c[0], c[1] ))
+    for dovidnik in dovidniks:
+        print("код ринка:{:3} назва ринку:{:3}"
+            .format(dovidnik[0],dovidnik[1]))
 
 
 def show_statistics(statistics):
@@ -71,7 +73,11 @@ def show_statistics(statistics):
         print("дата:{:5} код ринку:{:5} ціна за картоплю:{:5} ціна за капусту:{:5} ціна за цибулю:{:5}"
             .format(c[0], c[1], c[2], c[3], c[4]))
         
-    
+dovidniks = get_dovidniks()
+show_dovidniks(dovidniks)
+
+statistics = get_statistics()
+show_statistics(statistics)
 
 
 
