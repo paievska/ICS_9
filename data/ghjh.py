48d5ce3cd9e5f541185a2d74c920d1d45750af0f
Paievska <dianapaievska111@gmail.com>
2020-11-26 22:55:27 +0200

    commit

--- data/ghjh.py
+++ data/ghjh.py
@@ -1,10 +1,23 @@
-class Girls:
-    def __init__(self, name, age):
+class BankAccount:
+    def __init__(self, name, balance):
         self.name = name
-        self.age = age
+        self.balance = balance 
+ 
+    def __add__(self, other):
+        if isinstance(other, BankAccount):
+            return self.balance + other.balance
+        if isinstance(other, (int,float)):
+            return  self.balance + other
+        raise 
+    
+    def __sub__(self, other):
+        if isinstance(other, BankAccount):
+            return self.balance - other.balance
+        if isinstance(other, (int,float)):
+            return  self.balance - other
+        
+b = BankAccount('Bob', 500)
+c = BankAccount('Mary', 1200)
+print(b - 2)
+print(c - b)
 
-    def __repr__(self):
-        return f"Girls = (self.name)"  
-
-a = Girls('Mary', 20) 
-print(a.age)  
