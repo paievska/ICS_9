9ac3466124a20e1d18ddb099f19655bed6e17e22
Paievska <dianapaievska111@gmail.com>
2020-11-29 01:49:15 +0200

    cgkg

--- data/ghjh.py
+++ data/ghjh.py
@@ -1,23 +1,13 @@
-class BankAccount:
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance 
+class Words:
+    def __init__(self, letters):
+        self.letters = letters
  
-    def __add__(self, other):
-        if isinstance(other, BankAccount):
-            return self.balance + other.balance
-        if isinstance(other, (int,float)):
-            return  self.balance + other
-        raise 
-    
-    def __sub__(self, other):
-        if isinstance(other, BankAccount):
-            return self.balance - other.balance
-        if isinstance(other, (int,float)):
-            return  self.balance - other
+    def __len__(self):
+        return len(self.letters)
+
         
-b = BankAccount('Bob', 500)
-c = BankAccount('Mary', 1200)
-print(b - 2)
-print(c - b)
+b = Words('Bob')
+print(len(b))
+
+
 
